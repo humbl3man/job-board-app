b033df79e9a16e2485d206964f3f9637a54eeb2d
Konstantin Minevich <humbl3dev@gmail.com>
2023-02-25 13:30:48 -0800

    add error route (404)

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.status === 404}
+	<h1>Page Not Found</h1>
+	<a
+		href="/"
+		class="link">Navigate Home</a
+	>
+{/if}
+{@debug $page}
