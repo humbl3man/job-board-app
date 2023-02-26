0d55d8c8748de23d98b884013b6e572ae804803d
Konstantin Minevich <humbl3dev@gmail.com>
2023-02-26 15:45:00 -0800

    protected dashboard

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { auth } from '$lib/auth';
+	import { Button, Skeleton, Alert } from '@svelteuidev/core';
+	import { CrossCircled } from 'radix-icons-svelte';
+
+	$: loading = $auth === undefined;
+</script>
+
+{#if $auth !== null}
+	<div class="mx-auto max-w-7xl px-4">
+		<div class="dashboard">
+			<aside class="side-nav">
+				<Skeleton visible={loading}>
+					<Button
+						href="/dashboard"
+						variant="subtle"
+						color="indigo"
+						class="w-full rounded-none"
+						size="md">Dashboard</Button
+					>
+
+					<Button
+						variant="subtle"
+						color="pink"
+						size="md"
+						class="w-full rounded-none"
+						on:click={auth.logout}>Sign Out</Button
+					>
+				</Skeleton>
+			</aside>
+			<main class="content">
+				<Skeleton
+					visible={loading}
+					height={50}
+				>
+					<slot />
+				</Skeleton>
+			</main>
+		</div>
+	</div>
+{:else}
+	<div class="mx-auto max-w-md mt-20">
+		<Alert
+			title="Access Denied"
+			variant="outline"
+			radius="sm"
+			color="pink"
+			icon={CrossCircled}
+		>
+			You need to be logged in to view this page.<br />
+		</Alert>
+		<Button
+			class="mt-4"
+			color="pink"
+			variant="filled"
+			radius="sm"
+			href="/login">Login</Button
+		>
+	</div>
+{/if}
+
+<style lang="postcss">
+	.dashboard {
+		@apply grid min-h-[400px];
+		grid-template-columns: max-content 1fr;
+	}
+	.side-nav {
+		@apply border-r bg-white pt-8;
+	}
+	.content {
+		@apply bg-white px-8 py-16;
+	}
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { auth } from '$lib/auth';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		if (!$auth) {
-			goto('/login');
-		}
-	});
 </script>
 
-<h1>Dashboard</h1>
 {#if $auth}
-	<p>Welcome, {$auth.displayName || $auth.email}</p>
-	<button on:click={auth.logout}>Log Out</button>
+	<h1 class="text-3xl">Welcome, {$auth.displayName || $auth.email}</h1>
 {/if}
