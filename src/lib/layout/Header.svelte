9628b5574950f4e8c460433ee90eb552eabda2d3
Konstantin Minevich <humbl3dev@gmail.com>
2023-02-27 17:31:59 -0800

    header improvements

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -7,28 +7,40 @@
 		{
 			href: '/jobs',
 			label: 'Jobs'
+		},
+		{
+			href: '/about',
+			label: 'About'
 		}
 	];
 </script>
 
 <header class="bg-white border-b border-slate-300">
-	<div class="mx-auto max-w-screen-2xl py-4">
+	<div class="mx-auto max-w-screen-2xl py-4 px-2">
 		<div class="flex items-center justify-between">
 			<a
 				href="/"
 				class="uppercase font-bold text-2xl">{APP_NAME}</a
 			>
-			<nav>
+			<nav class="flex">
 				{#each links as { href, label }}
-					<a
+					<Button
 						{href}
-						class="text-slate-800 font-bold uppercase">{label}</a
+						target=""
+						variant="subtle"
+						color="indigo"
+						class="text-gray-700 mx-2 text-base font-bold">{label}</Button
 					>
 				{/each}
 			</nav>
 			<div class="flex items-center">
 				{#if $auth}
-					<a href="/dashboard">{$auth.displayName || $auth.email}</a>
+					<Button
+						target=""
+						variant="light"
+						color="indigo"
+						href="/dashboard">Account</Button
+					>
 				{:else if $auth === null}
 					<Button
 						variant="filled"
