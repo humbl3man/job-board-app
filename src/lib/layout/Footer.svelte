13e2d963edaa18e4fb481ea0cee7a52c8b22f190
Konstantin Minevich <humbl3dev@gmail.com>
2023-02-27 17:41:34 -0800

    update footer with attributions

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,5 +1,18 @@
 <script>
 	import { APP_NAME } from '$lib/meta';
+	let copyrightYear = new Date().getFullYear();
 </script>
 
-<footer>{APP_NAME}</footer>
+<footer class="py-6 bg-white">
+	<div class="mx-auto max-w-screen-xl px-2">
+		<div class="text-center text-sm text-gray-700">{copyrightYear} &copy; {APP_NAME}</div>
+		<div class="text-center">
+			<a
+				href="https://storyset.com/job"
+				class="text-xs text-gray-500"
+				target="_blank"
+				rel="noreferrer noopener">Job illustrations by Storyset</a
+			>
+		</div>
+	</div>
+</footer>
